4e3c194b3096e31687a4d5763bfcab6aad933f88
Florent <florentm.contact@gmail.com>
2026-02-05 17:12:22 +0100

    CPP04/ex01 Add more test to main.cpp

--- CPP04/ex01/src/main.cpp
+++ CPP04/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 18:05:58 by fmotte            #+#    #+#             */
-/*   Updated: 2026/02/05 16:47:21 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/02/05 17:10:42 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,18 @@ int main()
     const Animal* j = new Dog();
     const Animal* i = new Cat();
     
+    std::cout << i->getType() << "\n";
+    i->makeSound();
+    
+    std::cout << j->getType() << "\n";
+    j->makeSound();
+    
     delete j;//should not create a leak
     delete i;
     
     ///MY TEST
+    std::cout << "\n";
+    
     const Animal* c = new Cat();
     c->haveAnIdea("I am dead and alive in the same time", 0);
     std::cout << c->tellAnIdea(0) << "\n";
