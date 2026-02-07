80a206c509ac508a481bb1713bb08532cd9838ff
Florent <florentm.contact@gmail.com>
2026-02-07 17:17:20 +0100

    Correct CPP04/ex02 Remove brain from Animal Classe

--- CPP04/ex02/src/Animal.cpp
+++ CPP04/ex02/src/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 17:33:12 by fmotte            #+#    #+#             */
-/*   Updated: 2026/02/05 17:05:29 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/02/07 16:54:55 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,23 @@
 // ====================
 
 //Constructor Default
-Animal::Animal(): _type(""), _brain(new Brain()) {}
+Animal::Animal(): _type("") {}
 
 //Constructor
-Animal::Animal(std::string type): _type(type), _brain(new Brain()) {}
+Animal::Animal(std::string type): _type(type) {}
 
 //Destructor
-Animal::~Animal(){delete _brain;}
+Animal::~Animal(){}
 
 //Copy constructor
-Animal::Animal(const Animal &other): _type(other._type), _brain(new Brain(*other._brain)) {}
+Animal::Animal(const Animal &other): _type(other._type) {}
 
 //Copy assignment
 Animal& Animal::operator=(const Animal& other)
 {
     if (this == &other) 
         return *this;
-        
-    delete _brain;
-    _brain = new Brain(*other._brain);
+    _type = other._type;
     return *this;
 }
 
@@ -56,10 +54,3 @@ void Animal::makeSound(void) const{std::cout << "Animal make sound\n";}
 // == Geter & Seter  ==
 // ====================
 const std::string Animal::getType(void) const {return _type;}
-
-// ====================
-// == Added Methode  ==
-// ====================
-void Animal::haveAnIdea(std::string idea, unsigned int idx) const {_brain->setIdeas(idea, idx);}
-std::string Animal::tellAnIdea(unsigned int idx) const {return _brain->getIdeas(idx);}
-void Animal::printIdea(void) const {for (unsigned int i = 0; i < 100; i++) std::cout << "- " << tellAnIdea(i) << "\n";}
