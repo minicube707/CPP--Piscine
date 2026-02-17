a58f0338edb2629db9af5b996c65346e5e7e1dba
Florent <florentm.contact@gmail.com>
2026-02-17 18:28:40 +0100

    CPP06/ex00 In progress

--- CPP06/ex00/include/ScalarConverter.hpp
+++ CPP06/ex00/include/ScalarConverter.hpp
@@ -6,11 +6,12 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 17:59:46 by fmotte            #+#    #+#             */
-/*   Updated: 2026/02/16 18:17:59 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/02/17 18:24:58 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <string>
+# include <cstring>
 # include <iostream>
 
 class ScalarConverter
@@ -25,5 +26,7 @@ class ScalarConverter
     public:
         // Convert the given input string to all possible scalar types (char, int, float, double)
         static void convert(std::string input);
+
+        static int check_type(std::string string);
 };
 
--- CPP06/ex00/src/main.cpp
+++ CPP06/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 17:56:55 by fmotte            #+#    #+#             */
-/*   Updated: 2026/02/16 18:28:56 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/02/17 18:22:09 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ int main(int argc, char **argv)
         std::cout << "Error: Wrong number of argument.\nPlease enter only one argument.\n";
         return (0);
     }
+    
     ScalarConverter::convert(argv[1]);
 }
