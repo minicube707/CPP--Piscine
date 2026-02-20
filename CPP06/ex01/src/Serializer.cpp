192862286726949d5dac3ce924e899776f4c3c77
Florent <florentm.contact@gmail.com>
2026-02-20 19:22:02 +0100

    CPP06/ex01 Add comment to Serializer.cpp

--- CPP06/ex01/src/Serializer.cpp
+++ CPP06/ex01/src/Serializer.cpp
@@ -6,11 +6,21 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 18:17:51 by fmotte            #+#    #+#             */
-/*   Updated: 2026/02/20 19:08:22 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/02/20 19:19:44 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
 
-uintptr_t Serializer::serialize(Data* ptr) {return (uintptr_t)ptr;}
-Data* Serializer::deserialize(uintptr_t raw) {return (Data*) raw;}
+/*
+Casts in C++:
+
+- (type)
+    Classic C-style cast, simple but less explicit; can hide risky conversions.
+    
+- reinterpret_cast<type>
+    C++ explicit cast for low-level conversions (e.g., pointer ↔ integer); use with care.
+*/
+
+uintptr_t Serializer::serialize(Data* ptr) {return reinterpret_cast<uintptr_t>(ptr);}
+Data* Serializer::deserialize(uintptr_t raw) {return reinterpret_cast<Data*>(raw);}
