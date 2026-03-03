851f035c7f46b9300e95c17e4370cda8fa5c5a41
Florent <florentm.contact@gmail.com>
2026-03-03 11:36:31 +0100

    CPP08/ex02 fini

--- CPP08/ex02/include/MutantStack.hpp
+++ CPP08/ex02/include/MutantStack.hpp
@@ -3,16 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: florent <florent@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 15:04:10 by fmotte            #+#    #+#             */
-/*   Updated: 2026/03/02 22:53:08 by florent          ###   ########.fr       */
+/*   Updated: 2026/03/03 11:34:55 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 # include <stack>
+# include <list>
+# include <iostream>
 
 template <typename T>
 class MutantStack: public std::stack<T>
@@ -27,6 +29,20 @@ class MutantStack: public std::stack<T>
         //Use the iterator of deque
         iterator begin() {return this->c.begin();}
         iterator end() {return this->c.end();}
+
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        const_iterator begin() const { return this->c.begin(); }
+        const_iterator end() const { return this->c.end(); }
+
+        typedef
+        typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        reverse_iterator rbegin() { return this->c.rbegin(); }
+        reverse_iterator rend() { return this->c.rend(); }
+
+        typedef typename std::stack<T>::container_type::const_reverse_iterator
+        const_reverse_iterator;
+        const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+        const_reverse_iterator rend() const { return this->c.rend(); }
 };
 
 //Include after the declaration, else the compilator crash
--- CPP08/ex02/src/main.cpp
+++ CPP08/ex02/src/main.cpp
@@ -3,16 +3,77 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: florent <florent@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 15:04:16 by fmotte            #+#    #+#             */
-/*   Updated: 2026/03/02 22:25:42 by florent          ###   ########.fr       */
+/*   Updated: 2026/03/03 11:35:53 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
 int main()
-{
+{   
+    //Stack
+    std::cout << "\nSTACK\n";
+    MutantStack<int> mstack;
+    
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << mstack.top() << std::endl;
+
+    mstack.pop();
+
+    std::cout << mstack.size() << std::endl;
+    
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    
+    mstack.push(0);
+    
+    MutantStack<int>::reverse_iterator it = mstack.rbegin();
+    MutantStack<int>::reverse_iterator ite = mstack.rend();
+    
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+    
+    
+    //List
+    std::cout << "\nList\n";
+    std::list<int> list;
+    
+    list.push_front(5);
+    list.push_front(17);
+
+    std::cout << list.front() << std::endl;
+
+    list.pop_front();
+
+    std::cout << list.size() << std::endl;
+    
+    list.push_front(3);
+    list.push_front(5);
+    list.push_front(737);
+    
+    list.push_front(0);
+    
+    std::list<int>::iterator lit = list.begin();
+    std::list<int>::iterator lite = list.end();
+    
+    ++lit;
+    --lit;
+    while (lit != lite)
+    {
+        std::cout << *lit << std::endl;
+        ++lit;
+    }
     return 0;
 }
