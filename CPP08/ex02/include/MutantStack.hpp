29dcdc50bca5160422069c035481d8ae592a7c1e
Florent <florentm.contact@gmail.com>
2026-03-06 18:13:04 +0100

    CPP09/ex02 Move function to .tpp and keep prototype in header

--- CPP08/ex02/include/MutantStack.hpp
+++ CPP08/ex02/include/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 15:04:10 by fmotte            #+#    #+#             */
-/*   Updated: 2026/03/03 11:34:55 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/03/06 17:51:02 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,24 +25,22 @@ class MutantStack: public std::stack<T>
         MutantStack(const MutantStack& other): std::stack<T>(other) {}
         MutantStack& operator=(const MutantStack& other);
 
-        typedef typename std::stack<T>::container_type::iterator iterator;
         //Use the iterator of deque
-        iterator begin() {return this->c.begin();}
-        iterator end() {return this->c.end();}
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator begin();
+        iterator end();
 
         typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-        const_iterator begin() const { return this->c.begin(); }
-        const_iterator end() const { return this->c.end(); }
+        const_iterator begin() const;
+        const_iterator end() const;
 
-        typedef
-        typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
-        reverse_iterator rbegin() { return this->c.rbegin(); }
-        reverse_iterator rend() { return this->c.rend(); }
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        reverse_iterator rbegin();
+        reverse_iterator rend();
 
-        typedef typename std::stack<T>::container_type::const_reverse_iterator
-        const_reverse_iterator;
-        const_reverse_iterator rbegin() const { return this->c.rbegin(); }
-        const_reverse_iterator rend() const { return this->c.rend(); }
+        typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+        const_reverse_iterator rbegin() const;
+        const_reverse_iterator rend() const;
 };
 
 //Include after the declaration, else the compilator crash
--- CPP08/ex02/include/MutantStack.tpp
+++ CPP08/ex02/include/MutantStack.tpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   MutantStack.tpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: florent <florent@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 15:04:13 by fmotte            #+#    #+#             */
-/*   Updated: 2026/03/02 22:53:51 by florent          ###   ########.fr       */
+/*   Updated: 2026/03/06 17:49:52 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,27 @@ MutantStack<T>& MutantStack<T>::operator=(const MutantStack<T>& other)
         std::stack<T>::operator=(other); //use the overload of stack
     return (*this);
 }
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin() {return this->c.begin();}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end() {return this->c.end();}
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::begin() const { return this->c.begin(); }
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::end() const { return this->c.end(); }
+
+template <typename T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rbegin() { return this->c.rbegin(); }
+
+template <typename T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rend() { return this->c.rend(); }
+
+template <typename T>
+typename MutantStack<T>::const_reverse_iterator MutantStack<T>::rbegin() const { return this->c.rbegin(); }
+
+template <typename T>
+typename MutantStack<T>::const_reverse_iterator MutantStack<T>::rend() const { return this->c.rend(); }
