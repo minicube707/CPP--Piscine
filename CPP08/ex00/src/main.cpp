abc712a761cc379cf8698254b6d69337a759b8f2
Florent <florentm.contact@gmail.com>
2026-03-12 14:35:40 +0100

    CPP08/ex00 Fix syntax

--- CPP08/ex00/src/main.cpp
+++ CPP08/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 11:05:42 by fmotte            #+#    #+#             */
-/*   Updated: 2026/02/25 13:16:56 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/03/12 14:30:45 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,45 +26,45 @@ int main()
     
     std::cout << "\nVerctor:\n";
     std::vector<int> v(arr, arr + 9);
-    std::cout << (easyfind(v, 1) ? "" : "not") << "find\n";
-    std::cout << (easyfind(v, 9) ? "" : "not") << "find\n";
-    std::cout << (easyfind(v, 10) ? "" : "not") << "find\n";
-    std::cout << (easyfind(v, -1) ? "" : "not") << "find\n";
+    std::cout << (easyfind(v, 1) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(v, 9) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(v, 10) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(v, -1) ? "" : "not ") << "find\n";
 
 
     std::cout << "\nList:\n";
     std::list<int> l;
     for (int i = 0; i < 9; i++)
         l.push_back(arr[i]);
-    std::cout << (easyfind(l, 1) ? "" : "not") << "find\n";
-    std::cout << (easyfind(l, 9) ? "" : "not") << "find\n";
-    std::cout << (easyfind(l, 10) ? "" : "not") << "find\n";
-    std::cout << (easyfind(l, -1) ? "" : "not") << "find\n";
+    std::cout << (easyfind(l, 1) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(l, 9) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(l, 10) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(l, -1) ? "" : "not ") << "find\n";
 
     
     std::cout << "\nSet:\n";
     std::set<int> se(arr, arr + 9);
-    std::cout << (easyfind(se, 1) ? "" : "not") << "find\n";
-    std::cout << (easyfind(se, 9) ? "" : "not") << "find\n";
-    std::cout << (easyfind(se, 10) ? "" : "not") << "find\n";
-    std::cout << (easyfind(se, -1) ? "" : "not") << "find\n";
+    std::cout << (easyfind(se, 1) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(se, 9) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(se, 10) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(se, -1) ? "" : "not ") << "find\n";
 
 
     std::cout << "\nStack:\n";
     std::set<int> st(arr, arr + 9);
-    std::cout << (easyfind(st, 1) ? "" : "not") << "find\n";
-    std::cout << (easyfind(st, 9) ? "" : "not") << "find\n";
-    std::cout << (easyfind(st, 10) ? "" : "not") << "find\n";
-    std::cout << (easyfind(st, -1) ? "" : "not") << "find\n";
+    std::cout << (easyfind(st, 1) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(st, 9) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(st, 10) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(st, -1) ? "" : "not ") << "find\n";
 
     
     std::cout << "\nDequeue:\n";
     std::deque<int> d;
     for (int i = 0; i < 9; i++)
         d.push_back(arr[i]);
-    std::cout << (easyfind(d, 1) ? "" : "not") << "find\n";
-    std::cout << (easyfind(d, 9) ? "" : "not") << "find\n";
-    std::cout << (easyfind(d, 10) ? "" : "not") << "find\n";
-    std::cout << (easyfind(d, -1) ? "" : "not") << "find\n";
+    std::cout << (easyfind(d, 1) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(d, 9) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(d, 10) ? "" : "not ") << "find\n";
+    std::cout << (easyfind(d, -1) ? "" : "not ") << "find\n";
 
 }
