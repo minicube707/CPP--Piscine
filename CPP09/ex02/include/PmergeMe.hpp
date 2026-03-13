98a5020b8c151a88f684f491af4f70cd4935ee07
Florent <florentm.contact@gmail.com>
2026-03-13 19:49:43 +0100

    CPP09/ex02 Progress part 2

--- CPP09/ex02/include/PmergeMe.hpp
+++ CPP09/ex02/include/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 18:04:12 by fmotte            #+#    #+#             */
-/*   Updated: 2026/03/12 19:23:23 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/03/13 16:58:06 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,15 @@ class PmergeMe
         void fill_list();
 
         void merge_sort_vector(unsigned int level);
+        void insertion_vector(unsigned int peer_size);
+        
         unsigned int get_jacobsthal(const unsigned int n);
+        
+        std::vector<unsigned int>::iterator binary_search(
+            const unsigned int val, 
+            std::vector<unsigned int>::iterator begin, 
+            std::vector<unsigned int>::iterator end
+        );
 };
 
 
--- CPP09/ex02/src/PmergeMe.cpp
+++ CPP09/ex02/src/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 18:04:17 by fmotte            #+#    #+#             */
-/*   Updated: 2026/03/12 19:23:41 by fmotte           ###   ########.fr       */
+/*   Updated: 2026/03/13 19:46:26 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,11 +69,10 @@ void PmergeMe::merge_sort_vector(unsigned int level)
     unsigned int max_fisrt;
     unsigned int max_second;
     std::vector<int>::iterator mid_peer;
-    unsigned int peer_size = 1 << level;
+    const unsigned int peer_size = 1 << level;
     
     if (peer_size * 2 > _vector.size())
         return;
-        
 
     for (unsigned int i = 0; i + peer_size <= _vector.size(); i += peer_size)
     {  
@@ -96,6 +95,60 @@ void PmergeMe::merge_sort_vector(unsigned int level)
     std::cout << "\n\n";
     
     merge_sort_vector(++level);
+    insertion_vector(peer_size);
+}
+
+void PmergeMe::insertion_vector(const unsigned int peer_size)
+{
+    std::vector<unsigned int> vec_main;
+    std::vector<unsigned int> vec_rest;
+    std::vector<unsigned int> vec_val_max;
+    
+    unsigned int last_index = ((_vector.size() - 1) / peer_size) * peer_size;
+    vec_rest.insert(vec_rest.begin(), _vector.begin() + last_index, _vector.end());
+
+    //Add the first smallest peer
+    vec_main.insert(vec_main.begin(), _vector.begin(), _vector.begin() + peer_size);
+    
+    //Add the greater peer
+    for (unsigned int i = peer_size; i < last_index; i += 2 * peer_size)
+        vec_main.insert(vec_main.end(), _vector.begin() + i, _vector.begin() + i + peer_size);
+        
+    for (unsigned int i = peer_size; i <= vec_main.size(); i += peer_size)
+        vec_val_max.push_back(vec_main[i-1]);
+        
+    //insertion find the place for the smallest
+    
+    //Add the rest
+    //vec_main.insert(vec_main.end(), vec_rest.begin(), vec_rest.end());
+    
+    //delete
+    std::cout << "Peer Size: " << peer_size << "\n";
+
+    std::cout << "Val max\n";
+    for (unsigned int i = 0; i < vec_val_max.size(); ++i)
+        std::cout << vec_val_max[i] << " ";
+    std::cout << "\n";
+    
+    std::cout << "Rest\n";
+    for (unsigned int i = 0; i < vec_rest.size(); ++i)
+        std::cout << vec_rest[i] << " ";
+    std::cout << "\n";
+    
+    std::cout << "Smallest\n";
+    for (unsigned int i = 2 * peer_size; i < last_index; i += 2 * peer_size)
+    {
+        for (unsigned int j = 0; j < peer_size; j++)
+            std::cout << _vector[i+j] << " ";
+        std::cout << "Max local: " << _vector[i + peer_size - 1] << "\n";
+        std::cout << "place: " << binary_search(_vector[i + peer_size - 1], vec_val_max.begin(), vec_val_max.end()) - vec_val_max.begin() << "\n\n";
+    }
+    
+    std::cout << "MAIN\n";
+    for (unsigned int i = 0; i < vec_main.size(); ++i)
+        std::cout << vec_main[i] << " ";
+    std::cout << "\n";
+    std::cout << "\n";
 }
 
 unsigned int PmergeMe::get_jacobsthal(const unsigned int n)
@@ -108,4 +161,21 @@ unsigned int PmergeMe::get_jacobsthal(const unsigned int n)
         res -= 1;
         
     return res / 3;
+}
+
+std::vector<unsigned int>::iterator PmergeMe::binary_search(
+    const unsigned int val,
+    std::vector<unsigned int>::iterator begin,
+    std::vector<unsigned int>::iterator end
+)
+{
+    if (begin == end)
+        return begin;
+
+    std::vector<unsigned int>::iterator mid = begin + (end - begin) / 2;
+
+    if (*mid < val)
+        return binary_search(val, mid + 1, end);
+    else
+        return binary_search(val, begin, mid);
 }
